6ba67e54fd9fceb41cadc6407560e24b041a383e
alicia <66540464+maplicia@users.noreply.github.com>
2024-02-22 15:16:54 -0500

    add app files

---
+++ src/app.py
@@ -0,0 +1,49 @@
+
+#%% import pkgs
+#import packages
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
+import pandas as pd
+import plotly.express as px
+#%% assign data variables
+adf = pd.read_excel('gamechangerdata.xlsx', sheet_name='all')
+#%%init
+# Initialize the app
+app = Dash(__name__, update_title="Game Changer Data")
+server = app.server
+#%% app layout
+app.layout = html.Div([
+    dash_table.DataTable(id='datatable',
+                        data=adf.to_dict('records'), 
+                        editable=True,
+                        sort_action="native",
+                        page_size=8),
+    dcc.RadioItems(id = 'radio-items',
+                    options=[
+                {'label': 'Appearances', 'value': 'Appearances'},
+                {'label': 'Competitive Episodes', 'value': 'Competitive Episodes'},
+                {'label': 'Win Count', 'value': 'Win Count'},
+                {'label': 'Total Points', 'value': 'Total Points'}
+            ], 
+                        value='Appearances', 
+                        inline=True),
+    dcc.Graph(id='bar-chart'),
+])
+
+#%% callback
+@app.callback(
+    Output('bar-chart', 'figure'),
+    [Input('radio-items', 'value')]
+)
+def update_bar_chart(selected_y_axis):
+    fig = px.bar(adf, 
+                 x='Player', 
+                 y=selected_y_axis,
+                 text_auto=True)
+    fig.update_layout(xaxis={'categoryorder':'total descending'})
+
+    return fig
+#%% run app
+
+if __name__ == '__main__':
+    app.run(debug=True)
+# %%
